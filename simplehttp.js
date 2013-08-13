4049b506fb9cea4c941d56bae3674bf37bbe61f2
chad <chad.gilbert@forces.gc.ca>
2013-08-13 14:04:59 -0300

    Added a log file to log incoming requests. Now requires >= node 0.8

--- simplehttp.js
+++ simplehttp.js
@@ -1,10 +1,12 @@
 var http = require("http"); // Including HTTP module so we can create the server.
 var url  = require("url");  // Including URL module to parse request params.
+var fs   = require("fs");   // Including File-System module to read/write files.
 
 // Create the server. 
 http.createServer(function(request, response) {
     // Attach listener and event.
     request.on("end", function() {
+        
         // Parse the request for arguments and store them in "_get".
         var _get = url.parse(request.url, true).query;
 
@@ -15,5 +17,8 @@ http.createServer(function(request, response) {
         
         // Send data and end response.
         response.end('Here are your data: ' + _get['data']);
+        
+        // Log the request data.
+        fs.appendFile("request.log",_get['data']+"\n", function(){});
     });
 }).listen(8080);
