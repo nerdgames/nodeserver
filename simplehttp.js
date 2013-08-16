869fa9ef6823396cfd29d747f5cc9fccad263cff
chad <chad.gilbert@forces.gc.ca>
2013-08-16 12:21:03 -0300

    Switched to node v0.10 from v0.8. Fixed simplehttp.js to reflect that.

--- simplehttp.js
+++ simplehttp.js
@@ -1,12 +1,26 @@
+/*
+ * simplehttp - the most basic node.js server you can make
+ *   Uses node.js v0.10.15
+ *   Modules:
+ *     <none>
+ */
+
 var http = require("http"); // Including HTTP module so we can create the server.
 var url  = require("url");  // Including URL module to parse request params.
 var fs   = require("fs");   // Including File-System module to read/write files.
 
 // Create the server. 
 http.createServer(function(request, response) {
-    // Attach listener and event.
+    
+    // Let the user know things are happening.
+    console.log("Received a request.");
+  
+    // Respond to request, once complete. 
     request.on("end", function() {
         
+        // Announce that the request has been received. 
+        console.log("Request ended. Parsing...");
+
         // Parse the request for arguments and store them in "_get".
         var _get = url.parse(request.url, true).query;
 
@@ -19,6 +33,12 @@ http.createServer(function(request, response) {
         response.end('Here are your data: ' + _get['data']);
         
         // Log the request data.
-        fs.appendFile("request.log",_get['data']+"\n", function(){});
+        fs.appendFile("request.log",_get['data']+"\n", function(err){
+            if (err) console.log("Error writing to the requests log..."); 
+        });
     });
+
+    request.resume();
 }).listen(8080);
+
+console.log("Serving on localhost:8080.");
